3c51945aea15c572f691388a8b9461a0d66311d2
Evan James <thevan777@gmail.com>
2021-10-06 13:01:54 -0400

    alternating sums

--- app.js
+++ app.js
@@ -314,3 +314,49 @@
 //     }
 //     return inputString;
 // }
+
+
+//=================================================//
+//================ Alternating Sums ===============//
+//=================================================//
+
+// Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+
+// You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+// Example
+
+// For a = [50, 60, 60, 45, 70], the output should be
+// alternatingSums(a) = [180, 105].
+
+// Solution:
+
+// function alternatingSums(a) {
+//     let even = []
+//     let odd = []
+//     let solution = []
+//     for(let i = 0; i < a.length; i++){
+//         if(i % 2 === 0 || i === 0){
+//             even.push(a[i])
+//         } else {
+//             odd.push(a[i])
+//         }
+//     }
+    
+//     if(even.length > 0) {
+//         let num1 = even.reduce((a, b) => a + b)
+//         even = num1
+//     } else {
+//         even = 0
+//     }
+    
+//     if(odd.length > 0){
+//         let num2 = odd.reduce((a, b) => a + b)
+//         odd = num2
+//     } else {
+//         odd = 0
+//     }
+    
+//     solution.push(even, odd)
+//     return solution
+// }

c078a4cac6932b55385f7772d81fe99e5860587d
Evan James <thevan777@gmail.com>
2021-10-05 20:15:46 -0400

    common character count

--- app.js
+++ app.js
@@ -178,3 +178,29 @@
 //     }
 //     return longest
 // }
+
+
+//=================================================//
+//============= Common Character Count ============//
+//=================================================//
+
+
+// Given two strings, find the number of common characters between them.
+
+// Example
+
+// For s1 = "aabcc" and s2 = "adcaa", the output should be
+// commonCharacterCount(s1, s2) = 3.
+
+// Strings have 3 common characters - 2 "a"s and 1 "c".
+
+
+// Solution: 
+
+// function commonCharacterCount(s1, s2) {
+//     for (let i = 0; i < s1.length; i++) {
+//         s2 = s2.replace(s1[i], "!");
+//     }
+//     return s2.replace(/[^!]/g, "").length;
+    
+// }
